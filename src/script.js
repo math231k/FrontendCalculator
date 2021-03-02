758cc42782a5ae3be89d12e9c9296f90774f347b
math231k <54638811+math231k@users.noreply.github.com>
2021-03-02 20:15:54 +0100

    power

--- src/script.js
+++ src/script.js
@@ -2,8 +2,10 @@ function calculate() {
     var number1 = parseFloat($("#number1").val());
     var number2 = parseFloat($("#number2").val());
     var operator = $("#operator").val();
+    if(operator === "^"){
+        calculatePower(number1, number2)
+    }
     var equation = number1 + operator + number2;
-
     var result = eval(equation);
     $("#result").val(result);
 }
@@ -27,4 +29,11 @@ function calculateFactorial() {
     var n = parseInt($("#fact-number").val());
     var result = factorial(n);
     $("#fact-result").val(result);
+}
+
+function calculatePower(x, y){
+    x = parseInt($("#pow-number").val());
+    y = parseInt($("#exp-number"))
+    var result = Math.pow(x, y);
+    $("#pow-result").val(result);
 }
